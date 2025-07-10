442174caeaa40c77498bc5344a2a10c8804a9dc7
Stasn13 <stasn139@gmail.com>
2025-07-10 14:00:00 +0100

    router home page dummy content

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  return (
+    <div className="p-2">
+      <h3>Cards goes here</h3>
+    </div>
+  );
+}
