0dc8608374bd02701fe9e93e68987002c2bdff67
Stasn13 <stasn139@gmail.com>
2025-07-11 23:11:28 +0100

    refactoring after transition to new routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const router = createRouter({ routeTree });
 
@@ -15,7 +17,8 @@ declare module "@tanstack/react-router" {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useGetCards } from "../hooks/useGetCards";
+import { Input } from "@mui/material";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  const { data, isLoading } = useGetCards();
+
+  console.log(data);
   return (
     <div className="p-2">
       <h3>Cards goes here</h3>
+      <Input onChange={(e) => console.log(e.target.value)} />
     </div>
   );
 }
