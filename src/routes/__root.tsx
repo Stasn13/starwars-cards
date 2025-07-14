07934e156b9b9a2213ff13ac2c06a002d109dddc
Stasn13 <stasn139@gmail.com>
2025-07-14 23:01:16 +0100

    refine styles and add logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,24 @@
 import { Box } from "@mui/material";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import Logo from "../assets/Logo";
 
 export const Route = createRootRoute({
   component: () => (
     <Box
       component={"main"}
-      sx={{ padding: "2rem", maxWidth: "1280px", margin: "0 auto" }}
+      sx={{
+        padding: "2rem",
+        maxWidth: "1280px",
+        margin: "0 auto",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+      }}
     >
-      {/* TODO: some logo here */}
+      <Link to="/">
+        <Logo />
+      </Link>
       <Outlet />
       <TanStackRouterDevtools />
     </Box>
--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
       autoCodeSplitting: true,
     }),
     react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
