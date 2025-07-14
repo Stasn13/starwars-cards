864f1184bbf74736616c63a466108da0e1eda32d
Stasn13 <stasn139@gmail.com>
2025-07-14 21:46:44 +0100

    refactor types

--- src/hooks/useGetCards.ts
+++ src/hooks/useGetCards.ts
@@ -4,9 +4,17 @@ import { BASE_URL } from "../constants/api";
 export type Card = {
     properties: {
         name: string;
-        uid: string;
+        birth_year: string;
+        eye_color: string;
+        gender: string;
+        hair_color: string;
+        height: string;
+        mass: string;
+        skin_color: string;
         url: string;
     }
+    uid: string;
+}
 
 const fetchCards = async (search?: string): Promise<Card[]> => {
   const url = new URL(`${BASE_URL}/people/?expanded=true`); // TODO: take a look - ?expanded=true - probably solution to reduce requests
---
+++ src/types/Card.ts
@@ -0,0 +1,14 @@
+export type Card = {
+    properties: {
+        name: string;
+        birth_year: string;
+        eye_color: string;
+        gender: string;
+        hair_color: string;
+        height: string;
+        mass: string;
+        skin_color: string;
+        url: string;
+    }
+    uid: string;
+}   
